1a27a2bd6092e5365f68e90a8f2fe5e1f34d2970
Cu Cong Can <cancc@vitalify.asia>
2018-09-11 14:00:11 +0700

    KotlinPushApp using sdk v3.0.0

---
+++ app/src/main/java/com/nifcloud/mbaas/ncmbpushquickstart/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.nifcloud.mbaas.ncmbpushquickstart
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.nifcloud.mbaas.core.NCMB
+import com.nifcloud.mbaas.core.NCMBPush
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //**************** APIキーの設定とSDKの初期化 **********************
+        NCMB.initialize(this, "YOUR_APPLICATION_KEY", "YOUR_CLIENT_KEY")
+
+        setContentView(R.layout.activity_main)
+    }
+
+    public override fun onResume() {
+        super.onResume()
+
+        //リッチプッシュ通知の表示
+        NCMBPush.richPushHandler(this, intent)
+
+        //リッチプッシュを再表示させたくない場合はintentからURLを削除します
+        intent.removeExtra("com.nifcloud.mbaas.RichUrl")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+}
