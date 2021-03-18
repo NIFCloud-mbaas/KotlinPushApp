4316b8f683e454e0157051cc885e24eaf38ba0e5
HungNV <vfa.hungnv@gmail.com>
2021-03-18 14:51:21 +0700

    ticket #316551 UITest push notification.

--- app/src/androidTest/java/com/nifcloud/mbaas/ncmbpushquickstart/ExecuteUITest.kt
+++ app/src/androidTest/java/com/nifcloud/mbaas/ncmbpushquickstart/ExecuteUITest.kt
@@ -6,19 +6,50 @@ import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.UiDevice
+import androidx.test.uiautomator.UiObject2
+import androidx.test.uiautomator.Until
+import com.nifcloud.mbaas.ncmbpushquickstart.Utils.sendPushWithSearchCondition
+import org.junit.Assert
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.jvm.Throws
+
+private const val TIMEOUT = 150000L
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
 class ExecuteUITest {
+    private lateinit var device: UiDevice
+
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Before
+    fun init() {
+        device = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+    }
 
     @Test
     fun initialScreen() {
         Espresso.onView(ViewMatchers.withText("Hello world!"))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun clickOnSendNotification() {
+        sendPushWithSearchCondition()
+        device.openNotification()
+        device.wait(Until.hasObject(By.text(NOTIFICATION_TITLE)), TIMEOUT.toLong())
+        val title: UiObject2 = device.findObject(By.text(NOTIFICATION_TITLE))
+        val text: UiObject2 = device.findObject(By.text(NOTIFICATION_TEXT))
+        Assert.assertEquals(NOTIFICATION_TITLE, title.text)
+        Assert.assertEquals(NOTIFICATION_TEXT, text.text)
+        title.click()
     }
 }
---
+++ app/src/androidTest/java/com/nifcloud/mbaas/ncmbpushquickstart/Utils.kt
@@ -0,0 +1,41 @@
+package com.nifcloud.mbaas.ncmbpushquickstart
+
+import android.util.Log
+import com.nifcloud.mbaas.core.NCMBInstallation
+import com.nifcloud.mbaas.core.NCMBPush
+import com.nifcloud.mbaas.core.NCMBQuery
+import org.json.JSONArray
+import org.json.JSONException
+
+private const val TAG = "FcmService"
+const val NOTIFICATION_TITLE = "UITest push notification"
+const val NOTIFICATION_TEXT = "Thank you! We appreciate your business, and we’ll do our best to continue to give you the kind of service you deserve."
+const val NOTIFICATION_RICH_URL = "https://www.nifcloud.com/"
+
+object Utils {
+    fun sendPushWithSearchCondition() {
+        val installation = NCMBInstallation.getCurrentInstallation()
+        installation.getDeviceTokenInBackground { token, e ->
+            val query = NCMBQuery<NCMBInstallation>("installation")
+            query.whereEqualTo("deviceToken", token)
+            val push = NCMBPush()
+            push.setSearchCondition(query)
+            push.title = NOTIFICATION_TITLE
+            push.message = NOTIFICATION_TEXT
+            push.richUrl = NOTIFICATION_RICH_URL
+            try {
+                push.target = JSONArray("[android]")
+            } catch (jsonException: JSONException) {
+                jsonException.printStackTrace()
+            }
+            push.dialog = true
+            push.sendInBackground { e ->
+                if (e != null) {
+                    Log.d(TAG, "Send push fail")
+                } else {
+                    Log.d(TAG, "Send push success!")
+                }
+            }
+        }
+    }
+}
